2227ca8525c0234f04089cdabdf9b2241bb0761c
tripdragon <nickitdesign@gmail.com>
2023-10-06 17:58:45 -0400

    taste the rainbow

--- Season1/grlggg/src/App.js
+++ Season1/grlggg/src/App.js
@@ -1,121 +1,36 @@
-// // import { Canvas } from '@react-three/fiber'
-// // import { useGLTF, OrbitControls, ContactShadows } from '@react-three/drei'
-// // import { useControls } from 'leva'
-// 
-// 
-// // import { createRoot } from 'https://esm.sh/react-dom/client'
-// 
-// // import ReactDOM from 'https://esm.sh/react-dom'
-// 
-// // import { React } from "react";
-// // import { useState, useEffect, useRef } from "https://esm.sh/react";
-// 
-// import { React, useState, useEffect, useRef } from "https://esm.sh/react";
-// 
-// // import { React, useState, useEffect, useRef } from "https://esm.sh/react";
-// // import { useState, useEffect, useRef } from "react";
-// // import { React, Suspense, useRef, useState } from 'https://esm.sh/react'
-// import { Canvas, useFrame } from 'https://esm.sh/@react-three/fiber'
-// // import htm from 'https://esm.sh/htm'
-// 
-// 
-// 
-// // import { Mask, useMask, TransformControls, Float, Environment, OrbitControls, MeshDistortMaterial, ContactShadows, useGLTF } from '@react-three/drei'
-// import { Mask, useMask, TransformControls, Float, Environment, OrbitControls, MeshDistortMaterial, ContactShadows, useGLTF } from 'https://esm.sh/@react-three/drei'
-// 
-// import { useControls } from 'https://esm.sh/leva'
-// 
-// 
-// 
-// 
-// const MODELS = {
-//   Beech: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-beech/model.gltf',
-//   Lime: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-lime/model.gltf',
-//   Spruce: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-spruce/model.gltf'
-// }
-// 
-// // export default function App() {
-// //   const { model } = useControls({ model: { value: 'Beech', options: Object.keys(MODELS) } })
-// //   return (
-// //     <>
-// //       <header>This is a {model.toLowerCase()} tree.</header>
-// //       <Canvas camera={{ position: [-10, 10, 40], fov: 50 }}>
-// //         <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
-// //         <spotLight position={[50, 50, 10]} angle={0.15} penumbra={1} />
-// //         <group position={[0, -10, 0]}>
-// //           <Model position={[0, 0.25, 0]} url={MODELS[model]} />
-// //           <ContactShadows scale={20} blur={10} far={20} />
-// //         </group>
-// //         <OrbitControls />
-// //       </Canvas>
-// //     </>
-// //   )
-// // }
-// 
-// 
-// function Box(props) {
-//   // This reference will give us direct access to the mesh
-//   const meshRef = useRef()
-//   // Set up state for the hovered and active state
-//   const [hovered, setHover] = useState(false)
-//   const [active, setActive] = useState(false)
-//   // Subscribe this component to the render-loop, rotate the mesh every frame
-//   useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-//   // Return view, these are regular three.js elements expressed in JSX
-//   return (
-//     <mesh
-//       {...props}
-//       ref={meshRef}
-//       scale={active ? 1.5 : 1}
-//       onClick={(event) => setActive(!active)}
-//       onPointerOver={(event) => setHover(true)}
-//       onPointerOut={(event) => setHover(false)}>
-//       <boxGeometry args={[1, 1, 1]} />
-//       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-//     </mesh>
-//   )
-// }
-// 
-// export default function App() {
-//   const { model } = useControls({ model: { value: 'Beech', options: Object.keys(MODELS) } })
-//   return (
-//     <>
-//       <header>This is a {model.toLowerCase()} tree.</header>
-//       <Canvas camera={{ position: [-10, 10, 40], fov: 50 }}>
-//         <ambientLight />
-//         <pointLight position={[10, 10, 10]} />
-//         <Box position={[-1.2, 0, 0]} />
-//         <Box position={[1.2, 0, 0]} />
-//         <OrbitControls />
-//       </Canvas>
-//     </>
-//   )
-// }
-// 
-// function Model({ url, ...props }) {
-//   const { scene } = useGLTF(url)
-//   // <primitive object={...} mounts an already existing object
-//   return <primitive object={scene} {...props} />
-// }
-// 
-// // Silently pre-load all models
-// Object.values(MODELS).forEach(useGLTF.preload)
+
+// import * as THREE from 'three';
 
 
 
 
+import { useRef, useState, useEffect } from 'react'
 
-import { useRef, useState } from 'react'
 // import { useRef, useState } from 'https://esm.sh/react'
 // import { useRef, useState } from 'https://unpkg.com/react'
+
 import { Canvas, useFrame } from '@react-three/fiber'
+
 // import { OrbitControls } from '@react-three/drei'
 
 import { useControls } from 'leva'
 // import { useControls } from 'https://esm.sh/leva'
 
-import { useGLTF, OrbitControls, ContactShadows } from '@react-three/drei'
+import { useGLTF, OrbitControls, ContactShadows, useAnimations } from '@react-three/drei'
 
+import { useLoader } from '@react-three/fiber'
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+
+// import * as THREE from "three";
+
+// this pulls nothing
+// import * as THREE from "three";
+import {Vector3, AnimationMixer} from "three";
+// window.THREE = THREE;
+
+// debugger
 
 function Box(props) {
   
@@ -148,14 +63,72 @@ function Box(props) {
 
 const MODELS = {
   Moof: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-beech/model.gltf',
+  horsey1: 'public/horsey2.glb'
   // Lime: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-lime/model.gltf',
   // Spruce: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-spruce/model.gltf'
 }
 
+
+
 function Model({ url, ...props }) {
-  const { scene } = useGLTF(url)
+  // const { scene } = useGLTF(url)
+  
+  const group = useRef();
+  
+  // https://stackoverflow.com/a/70944199
+  const { scene, animations } = useGLTF(url, true)
+  
   // <primitive object={...} mounts an already existing object
-  return <primitive object={scene} {...props} />
+  
+  // const { actions, mixer } = useAnimations(animations, group);
+
+
+  // const { nodes, materials, animations } = useGLTF(url)
+  // const { ref, mixer, names, actions, clips } = useAnimations(animations);
+  const { ref, names, actions, clips } = useAnimations(animations);
+  
+  var gg = useAnimations(animations);
+
+  let mixer
+  if (animations.length) {
+      // mixer = new THREE.AnimationMixer(scene);
+      mixer = new AnimationMixer(scene);
+      animations.forEach(clip => {
+          const action = mixer.clipAction(clip)
+          action.play();
+      });
+  }
+  
+  useFrame((state, delta) => {
+      mixer?.update(delta)
+  })
+  
+    // debugger
+  // return <primitive object={scene} {...props} />  
+//   useEffect(() => {
+//     actions?.jump.play()
+//   })
+// 
+// debugger
+
+  // useEffect(() => {
+  //   // this does not handle nested
+  //   // actions.Animation.play();
+  // 
+  //   // for (var i = 0; i < array.length; i++) {
+  //   //   array[i]
+  //   // }
+  // 
+  //   actions.Animation.play();
+  // }, [mixer]);
+  // 
+  
+  
+  // debugger
+
+  // return <primitive ref={group} object={scene} dispose={null} {...props} />
+  return <primitive ref={ref} object={scene} dispose={null} {...props} />
+  // return <primitive object={scene} {...props} />
 }
 
 //           <ContactShadows scale={20} blur={10} far={20} />
@@ -166,8 +139,12 @@ function Model({ url, ...props }) {
 
 export default function App() {
   
-  const { model } = useControls({ model: { value: 'Moof', options: Object.keys(MODELS) } })
+  // debugger
+  // const { model } = useControls({ model: { value: 'Moof', options: Object.keys(MODELS) } })
+  const { model } = useControls({ model: { value: 'horsey1', options: Object.keys(MODELS) } })
 
+  // const gltfHorsey = useLoader(GLTFLoader, 'models/horsey2.glb')
+    // return <primitive object={gltf.scene} />
 
   return (
     <>
@@ -175,16 +152,19 @@ export default function App() {
 
     <div style={{ width: "100vw", height: "100vh" }}>
 
-    <Canvas camera={{ fov: 25, position: [-1, 50, 80]}}>
+    <Canvas camera={{ fov: 15, position: [9, 50, 80]}}>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[1.2, 0, 0]} />
+      <Box position={[-1.2, 4, 0]} />
 
-{/*
-  <Box position={[-1.2, 0, 0]} />
-  */}
-      <Model position={[0, 0.25, 0]} url={MODELS[model]} />
+      {/*
+        <Box position={[-1.2, 0, 0]} />
+        <Model position={[0, 0.25, 0]} url={MODELS[model]} />
+        <primitive object={gltfHorsey.scene} />
+        */}
+        <Model position={[0, 0.25, 0]} url={'models/horsey2.glb'} />
+        
 
       <OrbitControls />
     </Canvas>
